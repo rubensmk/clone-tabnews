5ac96b280120875a57c441ef672414bbf82fe705
Rubens Mititaka Kishimoto <rubenskishimoto@gmail.com>
2024-04-25 22:13:35 +0000

    Add lottie files

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
 function Home() {
-  return <h1>Rubens Mititaka Kishimoto</h1>;
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Em construção...</h1>
+      <Player
+        src="https://lottie.host/7d2fa5f2-f4c2-40b7-b7b5-1d3839a8bf74/H74upuCZ9Q.json"
+        className="player"
+        loop
+        autoplay
+        style={{ height: "800px", width: "800px" }}
+      />
+    </main>
+  );
 }
 
 export default Home;

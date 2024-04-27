798c6664947b88441c94b14ae1acd1248d279b93
Rubens Mititaka Kishimoto <rubenskishimoto@gmail.com>
2024-04-27 20:42:53 +0000

    add folder to unit and integration tests

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora");
+const calculadora = require("../../models/calculadora");
 
 test("a soma de 2 + 2 deveria retornar 4", () => {
   const resultado = calculadora.somar(2, 2);

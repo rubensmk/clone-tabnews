d47e60baf95ebbb6013d4522f835fea7f432261a
Rubens Mititaka Kishimoto <rubenskishimoto@gmail.com>
2024-09-18 02:57:00 +0000

    refactor: renaming test suite description

--- tests/integration/v1/migrations/post.test.js
+++ tests/integration/v1/migrations/post.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
 
 describe("POST to /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    describe("Running pending migrations", () => {
+    describe("Retrieving pending migrations", () => {
       test("For the first time", async () => {
         const response = await fetch(
           "http://localhost:3000/api/v1/migrations",
